9394a17bc89467b3d7f4e6775d9da7af002a1710
dimaberezovskyi <dmberezovsky@gmail.com>
2023-08-26 00:50:23 +0300

    delete unused class

--- conftest.py
+++ conftest.py
@@ -1,9 +1,35 @@
 import os
+from contextlib import contextmanager
+
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-desired_caps = {"platform": "Windows 10", "browserName": "chrome", "version": "73"}
+
+# Initialize the WebDriver options
+def init_driver_options():
+    opts = webdriver.ChromeOptions()
+    # ... (options setup)
+    # opts.add_argument("--headless")
+    opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--no-sandbox")
+    return opts
+
+
+# Create a function to create the WebDriver instance
+def create_driver():
+    try:
+        driver = webdriver.Chrome(
+            executable_path=ChromeDriverManager().install(),
+            options=init_driver_options(),
+        )
+    except Exception as e:
+        print(f"Failed to install ChromeDriver: {e}")
+        driver = webdriver.Chrome(
+            executable_path=get_driver_path(), options=init_driver_options()
+        )
+    driver.maximize_window()
+    return driver
 
 
 def get_driver_path(driver_name="chromedriver"):
@@ -13,39 +39,33 @@ def get_driver_path(driver_name="chromedriver"):
     return driver_path
 
 
-def create_driver():
-    opts = webdriver.ChromeOptions()
-    # ... (options setup)
-    # opts.add_argument("--headless")
-    opts.add_argument("--disable-dev-shm-usage")
-    opts.add_argument("--no-sandbox")
-
-    try:
-        driver = webdriver.Chrome(
-            executable_path=ChromeDriverManager().install(), options=opts
-        )
-    except Exception as e:
-        print(f"Failed to install ChromeDriver: {e}")
-        driver = webdriver.Chrome(executable_path=get_driver_path(), options=opts)
-    driver.maximize_window()
-    return driver
-
-
 def quit_driver(driver):
     if driver is not None:
         driver.quit()
 
 
-@pytest.fixture(scope="session", autouse=True)
+# Change scope to "function" to ensure separate drivers create for each test function to avoin issues
+# in multithread, but this increases time execution and memory usage
+@pytest.fixture(scope="session")
 def make_driver(request) -> webdriver.Remote:
     driver = None
 
     def _make_driver() -> webdriver.Remote:
         nonlocal driver
-        driver = (
-            create_driver()
-        )  # Use the utility function to create the driver instance
+        driver = create_driver()
         return driver
 
     yield _make_driver()
-    quit_driver(driver)  # Use the utility function to quit the driver
+    quit_driver(driver)
+
+
+# Command line options to specify the browser version
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser-version", action="store", default="116", help="Specify the browser version"
+    )
+
+
+@pytest.fixture(scope="session")
+def browser_version(request):
+    return request.config.getoption("--browser-version", "116")
