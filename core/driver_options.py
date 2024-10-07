b731cd4d7ca18ebbc976efda2e48b702d4acb630
dmy.berezovskyi <dmy.berezovskyi@foodtech.team>
2024-10-07 21:28:51 +0300

    fix ruff

--- core/driver_options.py
+++ core/driver_options.py
@@ -1,4 +1,3 @@
-import os
 import platform
 from selenium import webdriver
 from utils.logger import Logger, LogLevel
@@ -10,7 +9,7 @@ log = Logger(log_lvl=LogLevel.INFO).get_instance()
 def _shared_driver_options(options):
     options.add_argument("--start-maximized")
     options.add_argument("--disable-dev-shm-usage")
-    options.page_load_strategy = 'none' # disable waiting for fully load page
+    options.page_load_strategy = 'none'  # disable waiting for fully load page
     if platform.system() == "Linux":
         options.add_argument("--no-sandbox")
     log.info(f"Driver options {options.arguments}")
